b60b7d3952c61cfc89d96e85f1c0895c1c318a28
urielT12 <uriel.tayar.2020@gmail.com>
2024-06-05 00:33:47 +0300

    remarks for lesson 1
    add Documentation for functions

--- lesson1-OneLiners.py
+++ lesson1-OneLiners.py
@@ -1,17 +1,20 @@
 import os
 
-
+path = "C:\\Users\\uriel\\OneDrive\\מסמכים\\names.txt"
 def longest_name_in_file(path):
+    """prints the longest name that in the file"""
     with open(path,'r') as file:
         print(max(file,key=len).strip())
 
 
 def sum_of_names_length(path):
-        with open(path,'r') as file:
-            print(sum(len(line.strip()) for line in file))
+    """prints the sum of all the names length"""
+    with open(path,'r') as file:
+        print(sum(len(line.strip()) for line in file))
 
 
 def print_shortest_names(path):
+    """going through the file and prints the shortest name"""
     with open(path,'r') as file:
         lines = file.readlines()
         min_length = min(len(line.strip()) for line in lines)
@@ -19,19 +22,51 @@ def print_shortest_names(path):
 
 
 def new_length_file(path):
+    """create a new file and adds the names length in place of each name
+    used os module because of a permissions problem"""
     with open(path, 'r') as file:
         with open(os.path.join(os.path.expanduser('~'), 'name_length.txt'),'w') as new_file:
                 new_file.write('\n'.join([str(len(line.strip())) for line in file]))
 
 
+
 def all_words_in_length(path):
+    """receives from the user a number representing the length of a name and
+    prints all the names in the names.txt file that are of this length"""
     with open(path,'r') as file:
         length = input("Enter name length: ")
         print('\n'.join(line.strip() for line in file if len(line.strip()) == int(length)))
 
 
+def main():
+    print("longest name: ")
+    longest_name_in_file(path)
+    print("sum of lengths: ")
+    sum_of_names_length(path)
+    print(("shortest names: "))
+    print_shortest_names(path)
+    print("a new length file created")
+    new_length_file(path)
+    all_words_in_length(path)
 
 
 
 
-new_length_file("C:\\Users\\uriel\\OneDrive\\מסמכים\\names.txt")
+if __name__ == '__main__':
+    main()
+
+
+"""
+output:
+    longest name: 
+    Vladimir
+    sum of lengths: 
+    38
+    shortest names: 
+    Ed
+    Jo
+    a new length file created
+    Enter name length: 4
+    Hans
+    Anna
+"""
--- lesson4-Generators.py
+++ lesson4-Generators.py
@@ -2,33 +2,40 @@ import datetime
 
 
 def gen_secs():
+    """a generator that produce all seconds 0-60"""
     for i in range(60):
         yield i
 
 def gen_minutes():
+    """a generator that produce all minutes 0-60"""
     for i in range(60):
         yield i
 
 def gen_hours():
+    """a generator that produce all hours 0-24"""
     for i in range(24):
         yield i
 
 def gen_time():
+    """a generator that produce all possible time combination in a day"""
     for hour in gen_hours():
         for minute in gen_minutes():
             for second in gen_secs():
                 yield f"{hour:02}:{minute:02}:{second:02}"
 
 def gen_years(start=2024):
+    """a generator that produce all years number - from input year (or 2024) to now"""
     current_year = datetime.datetime.now().year
     for i in range(start, current_year + 1):
         yield i
 
 def gen_months():
+    """a generator that produce all month numbers - 1-12"""
     for i in range(1, 13):
         yield i
 
 def gen_days(month, leap_year=True):
+    """a generator that produce all days number in a month based on - is it a leap year and what month is it"""
     thirty_one = [1, 3, 5, 7, 8, 10, 12]
     thirty = [4, 6, 9, 11]
     if month in thirty_one:
@@ -45,6 +52,7 @@ def gen_days(month, leap_year=True):
             yield i
 
 def gen_date():
+    """a generator that produce all possible time and date combinations from a specific year"""
     for year in gen_years(2019):
         leap = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
         for month in gen_months():
@@ -53,12 +61,21 @@ def gen_date():
                 for t in gen_time():
                     yield f"{date} {t}"
 
+def main():
+    """"main function that prints the date every 1,000,000 iterations"""
+    date_generator = gen_date()
+    count = 0
+    while True:
+        try:
 
-date = gen_date()
-count = 0
-for i in date:
-    if count == 1000000:
-        print(i)
-        count = 0
-    count += 1
+            next_combination = next(date_generator)
 
+            if count == 1000000:
+                print(next_combination)
+                count = 0
+            count += 1
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
