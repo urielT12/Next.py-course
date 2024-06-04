34fbf700a3487dec94cdd72475e2ebccc9ee7ea0
urielT12 <uriel.tayar.2020@gmail.com>
2024-06-05 00:34:14 +0300

    remarks for lesson 2
    added Documentation for functions

--- lesson2-OOP.py
+++ lesson2-OOP.py
@@ -1,26 +1,30 @@
 
 
 class Animal:
-    def __init__(self,name,hunger=0,zoo_name="Hayaton"):
+    def __init__(self, name, hunger=0, zoo_name="Hayaton"):
+        """Initialize an Animal object."""
         self._name = name
         self._hunger = hunger
         self._zoo_name = zoo_name
 
-
-
     def get_name(self):
+        """Return the name of the animal"""
         return self._name
 
     def is_hungry(self):
+        """Check if the animal is hungry."""
         return self._hunger > 0
 
     def feed(self):
+        """Feed the animal"""
         self._hunger -= 1
 
     def talk(self):
+        """Make the animal talk - implemented in subclasses"""
         pass
 
 class Dog(Animal):
+    """Initialize a Dog object"""
     def __init__(self,name,hunger=0):
         super().__init__(name,hunger)
 
@@ -32,6 +36,7 @@ class Dog(Animal):
         print("There you go, sir!")
 
 class Cat(Animal):
+    """Initialize a Cat object"""
     def __init__(self,name,hunger=0):
         super().__init__(name,hunger)
 
@@ -43,6 +48,7 @@ class Cat(Animal):
         print("Meeeeow")
 
 class Skunk(Animal):
+    """Initialize a Skunk object"""
     def __init__(self,name,hunger=0,stink_count=6):
         super().__init__(name,hunger)
         self._stink_count = stink_count
@@ -55,7 +61,9 @@ class Skunk(Animal):
         print("Dear lord!")
 
 class Unicorn(Animal):
+
     def __init__(self,name,hunger=0):
+        """Initialize a Unicorn object"""
         super().__init__(name,hunger)
 
     def talk(self):
@@ -67,6 +75,7 @@ class Unicorn(Animal):
 
 class Dragon(Animal):
     def __init__(self,name,hunger=0,color="Green"):
+        """Initialize a Dragon object"""
         super().__init__(name,hunger)
         self._color = color
 
@@ -78,6 +87,7 @@ class Dragon(Animal):
         print("$@#$#@$")
 
 def main():
+    """creates objects by table and add them to a list"""
     Brownie = Dog("Brownie",10)
     Zelda = Cat("Zelda",3)
     Stinky = Skunk("Stinky")
@@ -89,6 +99,9 @@ def main():
     Clair = Unicorn("Clair", 80)
     McFly = Dragon("McFly", 80)
     zoo_lst = [Brownie,Zelda,Stinky,Keith,Lizzy,Doggo,Kitty,Stinky_Jr,Clair,McFly]
+
+    """for each animal in list: if hungry - feed till it isn't 
+    , make it talk and activate original ability"""
     for animal in zoo_lst:
         if animal.is_hungry():
             print(type(animal).__name__ + " " + animal.get_name())
